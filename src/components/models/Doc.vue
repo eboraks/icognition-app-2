8d649a3f2411f0f5e67cdabfcb631478627cc888
Eliran Boraks <eboraks@gmail.com>
2024-10-02 07:51:04 -0400

    Adding Doc model

---
+++ src/components/models/Doc.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+    class Doc {
+        id: string;
+        title: string | null;
+        url: string | null;
+        tldr: [string] | null;
+        publicationDate: Date | null;
+        llmServiceMeta: Object | null;
+        status: string | null;
+        updateAt: Date | null;
+        oneSentenceSummary: string | null;
+        summary_citations: [Object] | null;
+        is_about: string | null;
+        entities_and_concepts: [Object] | null; 
+        cosine_similarity: number | null;
+        image_url: string | null;
+        site_name: string | null;
+        html_elements: [Object] | null;
+
+        constructor(title: string | null, url: string) {
+            this.title = title;
+            this.url = url;
+        }
+    }
+    export default Doc
+</script>
---
+++ src/components/models/Question.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+    class DocumentQuestion {
+        question: string;
+        document_id: string | null;
+        project_id: string | null;
+        
+        constructor(question: string, document_id: string | null, project_id: string | null) {
+            this.question = question;
+            this.document_id = document_id;
+            this.project_id = project_id;
+        }
+    }
+    export default DocumentQuestion
+</script>
