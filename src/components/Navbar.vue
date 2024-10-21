26e43b4b892a434e51fad0b1265af9309d3e85eb
Ryan Varela <ryan.d.varela@gmail.com>
2024-10-21 10:46:44 -0400

    adding semicolons, updating to allow for refresh of current page.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -68,141 +68,108 @@
     </header>
 </template>
 <script lang="ts" setup>
-import useLogout from '@/composables/useLogout';
-import useSignin from '@/composables/useLogin';
-import user_state from '@/composables/getUser';
-import Route_Location from '@/components/models/RouteLocation.vue';
-import { useRouter } from 'vue-router';
-import { onBeforeMount, ref } from 'vue'; 
-import { auth } from '@/firebase/config'
+    import useLogout from '@/composables/useLogout';
+    import useSignin from '@/composables/useLogin';
+    import user_state from '@/composables/getUser';
+    import { useRouter } from 'vue-router';
+    import { computed, onBeforeMount, ref } from 'vue';
+    import { auth } from '@/firebase/config';
 
-const { error, logout } = useLogout()
-const { login_error, login, isPending, loginGoogle } = useSignin()
-let isError = false;
-const menu_app = ref();
-const menu_website = ref();
-let route_location = Route_Location.HOME as string;
-const router = useRouter();
+    const { error, logout } = useLogout();
+    const { login_error, login, isPending, loginGoogle } = useSignin();
+    const menu_app = ref();
+    const menu_website = ref();
+    const router = useRouter();
 
+    const toggleAppMenu = (event) => {
+        menu_app.value.toggle(event);
+    };
 
-onBeforeMount(async() => {
-    try {
-        route_location = router.currentRoute.value.name as string;
-    } catch (err) {
-        isError = true;
-        console.log("Error: ", err);
-    }
-})
+    const toggleWebsiteMenu = (event) => {
+        menu_website.value.toggle(event);
+    };
 
-const toggleAppMenu = (event) => {
-    menu_app.value.toggle(event);
-};
-
-const toggleWebsiteMenu = (event) => {
-    menu_website.value.toggle(event);
-};
-
-// Check if user is logged in. If so, redirect to library page
-// I ended up using this listener in Navbar because I wasn't able to redirect after login because of race condition. 
-// The router guard always executed before the user was fully logged in.
-auth.onAuthStateChanged((_user) => {
-    if (user_state.user) {
-        console.log('User is logged in. Current user is: ', user_state.user)
-        console.log(route_location);
-        if (route_location == Route_Location.DOCUMENTS || route_location == Route_Location.PROJECTS) {
-            router.push({ name: route_location });
-        } else if (route_location == Route_Location.HOME || route_location == undefined) {
-            router.push('documents');
+    // Check if user is logged in. If so, redirect to library page
+    // I ended up using this listener in Navbar because I wasn't able to redirect after login because of race condition. 
+    // The router guard always executed before the user was fully logged in.
+    auth.onAuthStateChanged(async (_user) => {
+        if (user_state.user) {
+            if (router.currentRoute.value.name == 'home') {
+                router.push({ name: 'documents' });
+            }
         }
-    }
-});
+    });
 
-
-const handleLogout = async () => {
-    try {
-        await logout().then(() => {
-            router.push('/');
-        }).catch(error => {
+    const handleLogout = async () => {
+        try {
+            await logout().then(() => {
+                router.push('/');
+            }).catch(error => {
+                console.log(error.value);
+            });
+        } catch (error) {
             console.log(error.value);
-        });
-    } catch (error) {
-        console.log(error.value);
-    }
-}
-
-const handleGoogleLogin = async () => {
-    try {
-        await loginGoogle().then(() => {
-            console.log('Login successful using Google: ', user_state.user);
-        }).catch(error => {
-            console.log(error);
-        });
-    } catch (error) {
-        console.log('Login failed using Google: ', user_state.user);
-    }
-}
-
-const handleAccount = async () => {
-    
-}
-
-const handleNotifications = async () => {
-    
-}
-
-const app_menu_items = ref([
-    // {
-    //     label: ' Notifications',
-    //     icon: 'pi pi-envelope',
-    //     badge: 2,
-    //     command: () => {
-    //         handleNotifications();
-    //     }
-    // },
-    // {
-    //     label: ' Account',
-    //     icon: 'pi pi-cog',
-    //     command: () => {
-    //         handleAccount();
-    //     }
-    // },
-    {
-        label: ' Sign Out',
-        icon: 'pi pi-sign-out',
-        command: () => {
-            handleLogout();
         }
     }
-]);
 
-let website_menu_items = ref([
-    {
-        label: ' Login with Google',
-        icon: 'pi pi-google',
-        command: () => {
-            handleGoogleLogin();
+    const handleGoogleLogin = async () => {
+        try {
+            await loginGoogle().then(() => {
+                console.log('Login successful using Google: ', user_state.user);
+            }).catch(error => {
+                console.log(error);
+            });
+        } catch (error) {
+            console.log('Login failed using Google: ', user_state.user);
         }
     }
-]);
 
-router.afterEach((to, from) => {
-    if (to.name == undefined || to.name == 'home') {
-        route_location = Route_Location.HOME;
-        document.body.classList.remove('route-app');
-        document.body.classList.add('route-website');
-    } else {
-        switch (to.name) {
-            case 'projects':
-                route_location = Route_Location.PROJECTS;
-                break;
-            case 'documents':
-                route_location = Route_Location.DOCUMENTS;
-                break;
-            default:
-                route_location = Route_Location.APP;
+    const handleAccount = async () => { }
+
+    const handleNotifications = async () => { }
+
+    const app_menu_items = ref([
+        // {
+        //     label: ' Notifications',
+        //     icon: 'pi pi-envelope',
+        //     badge: 2,
+        //     command: () => {
+        //         handleNotifications();
+        //     }
+        // },
+        // {
+        //     label: ' Account',
+        //     icon: 'pi pi-cog',
+        //     command: () => {
+        //         handleAccount();
+        //     }
+        // },
+        {
+            label: ' Sign Out',
+            icon: 'pi pi-sign-out',
+            command: () => {
+                handleLogout();
+            }
         }
-        document.body.classList.remove('route-website');
-        document.body.classList.add('route-app');
-    }
-});
+    ]);
+
+    let website_menu_items = ref([
+        {
+            label: ' Login with Google',
+            icon: 'pi pi-google',
+            command: () => {
+                handleGoogleLogin();
+            }
+        }
+    ]);
+
+    router.afterEach((to, from) => {
+        if (to.name == undefined || to.name == 'home') {
+            document.body.classList.remove('route-app');
+            document.body.classList.add('route-website');
+        } else {
+            document.body.classList.remove('route-website');
+            document.body.classList.add('route-app');
+        }
+    });
 </script>
--- src/composables/useLogin.js
+++ src/composables/useLogin.js
@@ -1,9 +1,9 @@
-import { ref } from "vue"
-import { auth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from '../firebase/config'
-import user_state  from './getUser'
+import { ref } from "vue";
+import { auth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from '../firebase/config';
+import user_state  from './getUser';
 
-const login_error = ref(null)
-const isPending = ref(false)
+const login_error = ref(null);
+const isPending = ref(false);
 
 const login = async(email, password) => {
     login_error.value = null
@@ -23,27 +23,28 @@ const login = async(email, password) => {
 }
 const provider = new GoogleAuthProvider();
 
-const loginGoogle = async () => {signInWithPopup(auth, provider).then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    //const credential = GoogleAuthProvider.credentialFromResult(result);
-    //const token = credential.accessToken;
-    // The signed-in user info.
-    user_state.user = result.user;
-    console.log("Google Login user state: ", user_state.user)
-    return true
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    console.error("Google Login Error: ", error)
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
-  
+const loginGoogle = async () => {
+    signInWithPopup(auth, provider).then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        //const credential = GoogleAuthProvider.credentialFromResult(result);
+        //const token = credential.accessToken;
+        // The signed-in user info.
+        user_state.user = result.user;
+        console.log("Google Login user state: ", user_state.user)
+        
+        return true;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+    }).catch((error) => {
+        // Handle Errors here.
+        console.error("Google Login Error: ", error)
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+    });
 }
 
 
